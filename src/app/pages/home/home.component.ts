93ee9f52da5cd5aabbbbfcc6654c15059b5d1af3
Dany Bteich <danybteich@outlook.com>
2025-08-04 16:40:36 +0200

    Add some of the Home functionalities

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { PropertyService } from '../../services/property.service';
 
 @Component({
   selector: 'app-home.component',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
+  providers: [PropertyService],
+  imports: [CommonModule, HttpClientModule]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  properties: any[] = [];
 
+  constructor(private propertyService: PropertyService) {}
+
+  ngOnInit(): void {
+    this.loadProperties();
+  }
+
+  loadProperties(): void {
+    this.propertyService.getProperties().subscribe(data => {
+      this.properties = data.items || [];
+    });
+  }
+
+  refreshProperties(): void {
+    this.loadProperties();
+  }
 }
---
+++ src/app/services/property.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface PropertyResponse {
+  items: any[];
+  page: number;
+  pages: number;
+  per_page: number;
+  total: number;
+}
+
+@Injectable({ providedIn: 'root' })
+export class PropertyService {
+  private apiUrl = 'http://127.0.0.1:5000/api/v1/properties';
+
+  constructor(private http: HttpClient) {}
+
+  getProperties(): Observable<PropertyResponse> {
+    return this.http.get<PropertyResponse>(this.apiUrl);
+  }
+}
